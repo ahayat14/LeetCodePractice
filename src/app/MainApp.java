47cb15a06f162cb2b188d4e13c4627b4cf603858
Abul Hayat <abulhayat611@gmail.com>
2022-06-23 22:43:16 +0600

    Solve LeetCode Problem

--- src/app/MainApp.java
+++ src/app/MainApp.java
@@ -1,21 +1,13 @@
 package app;
 
-import app.leetCode.AddTwoNumber;
-
-import java.util.Scanner;
+import app.leetCode.AddDigit;
 
 public class MainApp {
     public static void main(String[] args) {
-        System.out.println("This is a new line");
-        AddTwoNumber obj = new AddTwoNumber();
-
-        System.out.print("Type a number: ");
-        Scanner myObj = new Scanner(System.in);
-        int num1 = myObj.nextInt();
-        System.out.print("Type another number: ");
-        int num2 = myObj.nextInt();
-
-        System.out.println(obj.sum(num1, num2));
+        int num = 1132;
+        AddDigit obj = new AddDigit();
+        System.out.println(obj.addDigits(num));
     }
+
 
 }
