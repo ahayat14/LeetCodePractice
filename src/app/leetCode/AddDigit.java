bb255e9f9ba779b373c6a49e7277adab2d1e5304
Abul Hayat <abulhayat611@gmail.com>
2022-06-23 22:42:28 +0600

    Practice Abstract Class

---
+++ src/app/leetCode/AddDigit.java
@@ -0,0 +1,17 @@
+package app.leetCode;
+
+public class AddDigit {
+    public int addDigits(int num) {
+        int sum = 0;
+        while (num > 0) {
+            sum += num % 10;
+            num = num / 10;
+
+            if (num == 0 && sum > 9) {
+                num = sum;
+                sum = 0;
+            }
+        }
+        return sum;
+    }
+}
