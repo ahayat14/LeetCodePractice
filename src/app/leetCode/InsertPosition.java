eb8d99c3755ff4e2876c3169c225e8d3a57a40d7
Abul Hayat <abulhayat611@gmail.com>
2022-06-24 22:36:39 +0600

    Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.

---
+++ src/app/leetCode/InsertPosition.java
@@ -0,0 +1,13 @@
+package app.leetCode;
+
+public class InsertPosition {
+    public static int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] >= target)
+                return i;
+        }
+        return n;
+    }
+
+}
