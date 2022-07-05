555a39b24453583ce88f2e07c49e1b324d75f1f7
Abul Hayat <abulhayat611@gmail.com>
2022-07-05 15:37:03 +0600

    Data Structure Practice

---
+++ src/app/DataStructure/LinkLAccessEkenent.java
@@ -0,0 +1,19 @@
+package app.DataStructure;
+
+import java.util.LinkedList;
+
+public class LinkLAccessEkenent {
+    public static void main(String[] args) {
+        LinkedList<String> languages = new LinkedList<>();
+
+        // add elements in the linked list
+        languages.add("Python");
+        languages.add("Java");
+        languages.add("JavaScript");
+        System.out.println("LinkedList: " + languages);
+
+        // get the element from the linked list
+        String str = languages.get(1);
+        System.out.print("Element at index 1: " + str);
+    }
+}
